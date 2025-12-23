e57a2541205d3aee1fa10fc259a3b3e6c05693ba
Raunit Verma <ramgopal10258@gmail.com>
2025-12-23 20:57:21 +0530

    added deal download

--- backend/deals/admin.py
+++ backend/deals/admin.py
@@ -1,6 +1,26 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Deal
+
+
+class CampaignNameFilter(admin.SimpleListFilter):
+    title = 'Campaign Name'
+    parameter_name = 'campaign_name'
+
+    def lookups(self, request, model_admin):
+        """Return a list of tuples for the filter options"""
+        from campaigns.models import Campaign
+        campaigns = Campaign.objects.all().order_by('title').distinct()
+        return [(campaign.id, campaign.title) for campaign in campaigns]
+
+    def queryset(self, request, queryset):
+        """Filter the queryset based on the selected campaign"""
+        if self.value():
+            return queryset.filter(campaign_id=self.value())
+        return queryset
 
 
 @admin.register(Deal)
@@ -20,6 +40,7 @@ class DealAdmin(admin.ModelAdmin):
     list_filter = [
         'status',
         'payment_status',
+        CampaignNameFilter,
         'invited_at',
         'completed_at',
         'address_requested_at',
@@ -27,6 +48,7 @@ class DealAdmin(admin.ModelAdmin):
         'shipped_at',
         'delivered_at',
     ]
+    actions = ['download_selected_deals_csv']
     search_fields = [
         'campaign__title',
         'campaign__brand__name',
@@ -114,3 +136,54 @@ class DealAdmin(admin.ModelAdmin):
         return obj.influencer.user.username
 
     influencer_username.short_description = 'Influencer'
+
+    def download_selected_deals_csv(self, request, queryset):
+        """Download CSV with campaign name, influencer email and phone for selected deals"""
+        response = HttpResponse(content_type='text/csv; charset=utf-8')
+        response['Content-Disposition'] = 'attachment; filename="deals_export.csv"'
+
+        writer = csv.writer(response)
+        # Write header
+        writer.writerow([
+            'Campaign Name',
+            'Influencer Username',
+            'Influencer Email',
+            'Influencer Phone',
+            'Country Code',
+            'Status',
+            'Payment Status',
+            'Invited At',
+            'Accepted At',
+            'Completed At',
+        ])
+
+        # Write deal data
+        for deal in queryset.select_related('campaign', 'influencer__user', 'influencer__user_profile'):
+            phone_number = ''
+            country_code = ''
+
+            # Get phone number from user_profile (matching serializer pattern)
+            if deal.influencer and deal.influencer.user_profile:
+                phone_number = deal.influencer.user_profile.phone_number or ''
+                country_code = deal.influencer.user_profile.country_code or ''
+            elif deal.influencer and deal.influencer.user and hasattr(deal.influencer.user, 'user_profile') and deal.influencer.user.user_profile:
+                phone_number = deal.influencer.user.user_profile.phone_number or ''
+                country_code = deal.influencer.user.user_profile.country_code or ''
+
+            writer.writerow([
+                deal.campaign.title if deal.campaign else '',
+                deal.influencer.user.username if deal.influencer and deal.influencer.user else '',
+                deal.influencer.user.email if deal.influencer and deal.influencer.user else '',
+                phone_number,
+                country_code,
+                deal.status or '',
+                deal.payment_status or '',
+                deal.invited_at.strftime('%Y-%m-%d %H:%M:%S') if deal.invited_at else '',
+                deal.accepted_at.strftime('%Y-%m-%d %H:%M:%S') if deal.accepted_at else '',
+                deal.completed_at.strftime('%Y-%m-%d %H:%M:%S') if deal.completed_at else '',
+            ])
+
+        self.message_user(request, f'Exported {queryset.count()} deal(s) to CSV.')
+        return response
+
+    download_selected_deals_csv.short_description = 'Download selected deals as CSV (with influencer email & phone)'
